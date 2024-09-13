ddafd7a0ae17efacc5cc04daea26d11e44baf474
HanaDoubleU <hanawasif@Hanas-MacBook-Pro-2.local>
2024-09-13 01:18:05 -0400

    beginning for the sake of beginning

---
+++ EmployeeSQL/datamodeling/schemaforerd.sql
@@ -0,0 +1,45 @@
+-- schema for erd
+-- source: schema.sql from tuesday's tenth activity
+-- source: slides for 9.3
+-- source: video on quickdbd
+
+departments
+-- # each department's number and its corresponding name
+--
+dept_no varchar(4) pk
+dept_name varchar(18)
+
+dept_emp
+-- # each employee and their corresponding department
+--
+emp_no int pk
+dept_no varchar(4) fk >- departments.dept_no
+
+dept_manager
+-- # each manager among employees and their corresponding department
+--
+dept_no varchar(4) fk >- departments.dept_no
+emp_no int pk
+
+employees
+-- # each employee's personal information
+--
+emp_no int pk
+emp_title_id varchar(5) fk >- titles.title_id
+birth_date varchar(10)
+first_name varchar(30)
+last_name varchar(30)
+sex varchar(1)
+hire_date varchar(10)
+
+salaries
+-- # each employee's salary
+--
+emp_no int pk
+salary int
+
+titles
+-- # each title's number and its corresponding name
+--
+title_id varchar(5) pk
+title varchar(18)
